be315ba395b250536ad98a895ecd9cbab0f3dcdb
Veronica Pak <veronica.pak@toshibagcs.com>
2023-08-30 08:21:19 -0400

    Spring-01-Core-IoCSpring-02-Core-Wiring

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,23 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp
+{
+  public static void main(String[] args)
+  {
+    ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+
+    FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+    ft.createAccount();
+
+//    PartTimeMentor pt = container.getBean("p1", PartTimeMentor.class);
+    PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+
+    pt.createAccount();
+
+    String str = container.getBean(String.class);
+    System.out.println( str);
+  }
+}
