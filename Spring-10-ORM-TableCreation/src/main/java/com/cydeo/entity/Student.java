c288955b9959ba1b23e8f7acd59062dae2dbd197
Veronica Pak <nikampak@gmail.com>
2023-10-23 11:14:14 -0400

    [Day 19] Spring ORM Part1

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -0,0 +1,13 @@
+package com.cydeo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Student {
+    @Id
+    private long id;
+    private String firstName;  //in SQL - first_name
+    private String lastName;
+    private String email;
+
+}
