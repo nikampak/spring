1ecf0348305a893a3a10fa4dbc3da18f31488b71
Veronica Pak <nikampak@gmail.com>
2023-10-30 07:56:13 -0400

    [Day 22] Spring ORM Part4

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Payment.java
@@ -0,0 +1,39 @@
+package com.cydeo.model;
+
+import com.cydeo.enums.Status;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+public class Payment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createDate;
+    private BigDecimal amount;
+    private Status paymentStatus;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private PaymentDetail paymentDetail;
+
+    @ManyToOne
+    private Merchant merchant;
+
+    @ManyToOne
+    private Customer customer;
+
+    public Payment(LocalDate createDate, BigDecimal amount, Status paymentStatus) {
+        this.createDate = createDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+}
