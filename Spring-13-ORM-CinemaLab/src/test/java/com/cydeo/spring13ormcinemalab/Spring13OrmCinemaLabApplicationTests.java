5e8946c3455921d1a6fe64d8d1b27f25d94d3830
Veronica Pak <nikampak@gmail.com>
2023-10-30 21:54:15 -0400

    [Day 23] Spring ORM - Cinema Lab

---
+++ Spring-13-ORM-CinemaLab/src/test/java/com/cydeo/spring13ormcinemalab/Spring13OrmCinemaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring13ormcinemalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring13OrmCinemaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
