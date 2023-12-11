9abcb85ae2981416c4aeb9e3431db6a31f10a31e
Veronica Pak <nikampak@gmail.com>
2023-12-11 10:31:53 -0500

    [Day 35] Spring Rest Part-1

---
+++ Spring-15-REST/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByCategory(String category);
+
+}
